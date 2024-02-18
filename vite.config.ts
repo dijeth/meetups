738e3bbc28440905883a19fe950f95ec8889c6a3
Dmitry Orlov <d.orlov777@gmail.com>
2024-02-18 14:35:08 +0300

    fix: api url to localhost

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://course-vue.javascript.ru',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
